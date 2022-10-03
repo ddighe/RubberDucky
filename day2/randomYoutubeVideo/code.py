cf9a1395d3471fb66c15b82dab92034b93c8875d
--show-scope <deven.dighe@gmail.com>
2022-10-03 12:53:52 -0400

    having trouble with ssl

--- day2/randomYoutubeVideo/code.py
+++ day2/randomYoutubeVideo/code.py
@@ -1,9 +1,11 @@
 
-
-
-import httplib
-
+from urllib.request import urlopen
 #conn = httplib.HTTPConnection
 
+
+url = 'https://random-ize.com/random-youtube/'
+
+a = urlopen(url)
+print(a)
 
 # https://stackoverflow.com/questions/2146383/https-connection-python
